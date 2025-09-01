26d59a586444de640c518fb0c9c3934b4b5d89f9
20192726 <20192726@fue.edu.eg>
2025-09-01 03:35:37 +0300

    feat: setup notifications feature with Clean Architecture and Riverpod state management

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:notification_counter/main.dart';
 import 'package:notification_counter/my_app.dart';
 
 void main() {
